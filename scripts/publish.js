b0abde87d9cfea38a18831efdc59186c0fc2db8f
Benjamin Pagelsdorf <bpagelsdorf@inovex.de>
2024-01-18 16:25:05 +0100

    improve release process

---
+++ scripts/publish.js
@@ -0,0 +1,33 @@
+const shell = require('shelljs');
+const fs = require('fs');
+const path = require('path');
+const semver = require('semver');
+
+const packagesToPublish = [
+  'elements',
+  'elements-angular',
+  'elements-react',
+  'elements-vue',
+]
+
+const currentBranch = shell.exec('git branch --show-current').trim();
+if (currentBranch !== 'master') {
+  shell.echo('Sorry, release is only on branch "master" allowed!');
+  shell.exit(1)
+}
+
+const VERSION = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json',)).toString())['version'];
+const isPreRelease = (semver.prerelease(VERSION) ?? []).length > 0
+
+if (!isPreRelease) {
+  shell.exec(`nx release version ${VERSION}`)
+  shell.exec(`git commit -m "chore: publish ${VERSION}" && git push --follow-tags`)
+} else {
+  shell.exec(`git tag v${VERSION} && git push origin ${VERSION}`)
+}
+
+// deploy packages to npm
+const npmTag = isPreRelease ? 'canary' : 'latest'
+packagesToPublish.forEach((npmPackage) => {
+  shell.exec(`npm publish -w packages/${npmPackage} --tag ${npmTag}`)
+})
